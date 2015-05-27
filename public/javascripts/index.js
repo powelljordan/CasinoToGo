c12ffa0d29123ac136fcc82fd1c9b6784cb8734f
Jordan <powellj@mit.edu>
2015-05-27 02:34:11 -0400

    Added some demo links in the moves section

---
+++ public/javascripts/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("#abanico").click(function(){
+		$("#videoFrame").attr('src', 'https://www.youtube.com/embed/24hYl3f21Ss');
+	});
+	$("#balsero").click(function(){
+		$("#videoFrame").attr('src', 'https://www.youtube.com/embed/OSA_k8AGJf8');
+	});
+	$("#croqueta").click(function(){
+		$("#videoFrame").attr('src', 'https://www.youtube.com/embed/-ocL7i5erW8');
+	});
+	$("#dame directo").click(function(){
+		$("#videoFrame").attr('src', 'https://www.youtube.com/embed/tk6Ey5HSmWw');
+	});
+	$("#evelyn").click(function(){
+		$("#videoFrame").attr('src', 'https://www.youtube.com/embed/eqr0OwhDpxE');
+	});
+});
