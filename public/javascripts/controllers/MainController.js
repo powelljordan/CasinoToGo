0e169475459453feb2bda1099560c9a7061153f4
Jordan <powellj@mit.edu>
2015-06-03 17:06:29 -0400

    Finished menu navigation and some showing hiding stuff. Data is completely off demoDB. I still need to add features such as editing and deleting moves when the admin mode, but it basically works

--- public/javascripts/controllers/MainController.js
+++ public/javascripts/controllers/MainController.js
@@ -8,9 +8,23 @@ myApp.config(function($sceDelegateProvider) {
 
 
 myApp.controller('MainController', ['$scope',
+
+
 	function($scope, $sce){
+		//Connect to the different parts of the database
+		$scope.newOptions = false;
+		$scope.movesTitle = "Moves";
+
 		var ref = new Firebase('https://resplendent-inferno-2685.firebaseio.com/');
-		$("#submit").click(function(){
+		var allDataDB = new Firebase('http://resplendent-inferno-2685.firebaseio.com/allData');
+		var startsWithDB = new Firebase('http://resplendent-inferno-2685.firebaseio.com/startsWith/');
+		var levelDB = new Firebase('http://resplendent-inferno-2685.firebaseio.com/level/');
+		var ruedaDB = new Firebase('http://resplendent-inferno-2685.firebaseio.com/rueda/');
+
+		//When you input new move information this function pushes the information to the appropriate parts of the
+		//database based on the fields of the move
+		$scope.pushDB = function(){
+			//get the text from the text input boxes
 			var titleValue = $("#titleInput").val();
 			$("#titleInput").val('');
 
@@ -26,37 +40,148 @@ myApp.controller('MainController', ['$scope',
 			var urlValue = $("#url").val();
 			var url = $("#url").val('');
 
-			ref.push({title: titleValue, startsWithValue: startsWithValue, level: levelValue, rueda:ruedaValue, url:urlValue})
+			var ref = new Firebase('https://resplendent-inferno-2685.firebaseio.com/');
+			var allDataDB = new Firebase('http://resplendent-inferno-2685.firebaseio.com/allData');
+			var startsWithDB = new Firebase('http://resplendent-inferno-2685.firebaseio.com/startsWith/' + startsWithValue.toString());
+			var levelDB = new Firebase('http://resplendent-inferno-2685.firebaseio.com/level/' + levelValue.toString());
+			var ruedaDB = new Firebase('http://resplendent-inferno-2685.firebaseio.com/rueda/' + ruedaValue.toString());
+
+			allDataDB.push({title: titleValue, startsWithValue: startsWithValue, level: levelValue, rueda:ruedaValue, url:urlValue, sel: false});
+			startsWithDB.push({title: titleValue, url: urlValue, sel: false});
+			levelDB.push({title: titleValue, url: urlValue, sel: false});
+			ruedaDB.push({title: titleValue, url: urlValue, sel: false});
 			console.log("pushed!");
 
-		});
 
+
+		};
+		//To ensure that only one item can be selected at a time we keep track of the last selected item so we can unselect it when
+		//we select a new one
 		previouslySelected = null;
+		var keys = testKeys;
 		$scope.moves = movesDB;
+		$scope.keys = testKeys;
+		$scope.allDataMoves = []
+		$scope.startsWithMoves = [];
+		$scope.levelMoves = [];
+		$scope.ruedaMoves = []
+		$scope.queriedMoves = qMoves;
+
+		//Changes the content in the moves array which basically changes what's shown in the list-view
+		$scope.updateMoves = function(newMoves){
+			$scope.queriedMoves.push.apply($scope.queriedMoves, newMoves);
+			console.log($scope.queriedMoves)
+			$scope.moves.length = 0;
+			$scope.moves.push.apply($scope.moves, newMoves);
+		}
+
+		//Same move method just doesn't update the queried moves. This is just to get it to work and should be remdied later
+		$scope.updateMovesWithoutSaving = function(newMoves){
+			$scope.moves.length = 0;
+			$scope.moves.push.apply($scope.moves, newMoves);
+		}
+
+		$scope.getKeys = function(){
+			$scope.keys.length = 0;
+			for (var k in $scope.moves["0"]){
+				if (k != "$$hashKey" && k.toLowerCase() != "none"){
+					$scope.keys.push.apply($scope.keys, [k]);
+				}
+			}
+		}
+
+		$scope.updateMovesWithCategoryChange = function(key){
+			a = [];
+			for (k in $scope.moves['0'][key]){
+				a.push($scope.moves['0'][key][k])
+			}
+			$scope.moves.length = 0;
+			$scope.moves.push.apply($scope.moves, a);
+		}
+
+
+		//Logic that handles making sure that only one item is selected by looking at the previously selected item
 		$scope.toggle = function(index){
-			if(previouslySelected != null){
+			if(previouslySelected != null && $scope.moves[index].title != $scope.moves[previouslySelected].title){
 				$scope.moves[previouslySelected].sel = false;
 			}
+
 			previouslySelected = index;
 		}
+		//Default video. I should connect this to the DB. It will eventually show a performance video (probably our latest)
 		$scope.link = "https://www.youtube.com/embed/Hu_mtYueXys";
 		$scope.updateVideoLink = function(index){
 			$("#videoFrame").attr('src', $scope.moves[index].url);
+			
 		}
 
-
-		ref.on('child_added', function(snapshot){
+		//Adds new moves to the allDataMoves array
+		allDataDB.on('child_added', function(snapshot){
 			var move = snapshot.val();
-			$scope.moves = $scope.moves.concat([move]);
-			$scope.$apply();
-			console.log($scope.moves);
+			$scope.allDataMoves.push(move);
+			$scope.updateMovesWithoutSaving($scope.allDataMoves);
 		})
 
+		//Adds new moves to the startsWithMoves array
+		startsWithDB.on('value', function(snapshot){
+			var move = snapshot.val();
+			if (move != null){
+				$scope.startsWithMoves.push(move);
+			}
+			// $scope.updateMovesWithoutSaving($scope.startsWithMoves);
+		})
+
+		//Adds new moves to the levelMoves array
+		levelDB.on('value', function(snapshot){
+			var move = snapshot.val();
+			if (move != null){
+				$scope.levelMoves.push(move);
+			}
+			// $scope.updateMovesWithoutSaving($scope.levelMoves);
+		})
+
+
+		//Adds new moves to the ruedaMoves array
+		ruedaDB.on('value', function(snapshot){
+			var move = snapshot.val();
+			if (move != null){
+				$scope.ruedaMoves.push(move);
+			}
+			// $scope.updateMovesWithoutSaving($scope.ruedaMoves);
+		})
+
+		//Removes the last item added to the moves array, this is not a good delete method and needs to be fixed
 		ref.on('child_removed', function(snapshot){
 			$scope.moves.pop();
 			$scope.$apply();
-			console.log($scope.moves);
 		})
 
+
+		$scope.searchForMove = function(){
+			console.clear();
+			var searchedMove = $("#searchBox").val();
+			if(searchedMove === "mitrueda"){
+				$("#moveInput").show();
+			}
+			else if(searchedMove === "hidemitrueda"){
+				$("#moveInput").hide();
+			}
+			else{
+				for(i in $scope.allDataMoves){
+					var move = $scope.allDataMoves[i];
+					if (move.title.toLowerCase() === searchedMove.toLowerCase()){
+						alert("Found it!");
+					}
+				}
+			}
+		}
+
+		$scope.hideMoves = function(){
+			$("#moves").hide();
+		}
+
+		$scope.showMoves = function(){
+			$("#moves").show();
+		}
 
 	}]);
--- public/javascripts/demoDB.js
+++ public/javascripts/demoDB.js
@@ -1,7 +1,13 @@
-movesDB = [
+movesDB1 = [
 	{title: "Abanico", startWith: "Sombrero", level: "intermediate", rueda: "no", url: 'https://www.youtube.com/embed/24hYl3f21Ss', sel: false},
 	{title: "Balsero", startWith: "Sombrero", level: "intermediate", rueda: "no", url: 'https://www.youtube.com/embed/OSA_k8AGJf8', sel: false},
 	{title: "Croqueta y Apirina", startWith: "", level: "beginner", rueda: "no", url: 'https://www.youtube.com/embed/-ocL7i5erW8', sel: false},
 	{title: "Dame Directo", startWith: "", level: "beginner", rueda: "no", url: 'https://www.youtube.com/embed/tk6Ey5HSmWw', sel: false},
 	{title: "Evelyn", startsWith: "Adios", level: "beginner", rueda: "no", url: 'https://www.youtube.com/embed/eqr0OwhDpxE', sel: false}
-]
+];
+
+movesDB = [];
+
+
+testKeys = [];
+qMoves = [];
